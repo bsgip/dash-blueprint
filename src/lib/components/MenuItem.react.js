6026837a6d45e0424c3093d7d0d0bc6e0a25be4f
Benjamin Weise <ben@repositpower.com>
2018-11-28 16:20:32 +1100

    update location using same idea as dcc.Link

--- src/lib/components/MenuItem.react.js
+++ src/lib/components/MenuItem.react.js
@@ -3,6 +3,32 @@ import PropTypes from 'prop-types';
 import { MenuItem as BPMenuItem} from "@blueprintjs/core";
 // import {Alignment} from '@blueprintjs/core/src/common/alignment';
 
+
+
+/*
+ * event polyfill for IE
+ * https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+ */
+function CustomEvent(event, params) {
+    // eslint-disable-next-line no-param-reassign
+    params = params || {
+        bubbles: false,
+        cancelable: false,
+        // eslint-disable-next-line no-undefined
+        detail: undefined,
+    };
+    const evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(
+        event,
+        params.bubbles,
+        params.cancelable,
+        params.detail
+    );
+    return evt;
+}
+CustomEvent.prototype = window.Event.prototype;
+
+
 /**
  * This is pretty much a straight copy/paste from the dash html component button,
  * except we render a blueprint button
@@ -21,15 +47,15 @@ export default class MenuItem extends React.Component {
         console.log('updating location!');
         // prevent anchor from updating location
         e.preventDefault();
-        // const {href, refresh} = this.props;
-        // if (refresh) {
-        //     window.location.pathname = href;
-        // } else {
-        //     window.history.pushState({}, '', href);
-        //     window.dispatchEvent(new CustomEvent('onpushstate'));
-        // }
-        // // scroll back to top
-        // window.scrollTo(0, 0);
+        const {href, refresh} = this.props;
+        if (refresh) {
+            window.location.pathname = href;
+        } else {
+            window.history.pushState({}, '', href);
+            window.dispatchEvent(new CustomEvent('onpushstate'));
+        }
+        // scroll back to top
+        window.scrollTo(0, 0);
     }
 
     render() {
