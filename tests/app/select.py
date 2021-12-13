c725b8abdba0fd024949fde62c84a154fa20b171
bennyweise <ben.weise@gmail.com>
2021-12-13 22:00:01 +1100

    Add tests for Select

---
+++ tests/app/select.py
@@ -0,0 +1,63 @@
+import dash_blueprint as dbp
+import dash
+from dash.dependencies import Input, Output
+import dash_html_components as html
+
+from time import sleep
+
+app = dash.Dash(__name__)
+
+app.scripts.config.serve_locally = True
+app.css.config.serve_locally = True
+
+app.layout = html.Div(
+    [
+        dbp.FormGroup(
+            id="formgroup",
+            required=True,
+            children=[
+                dbp.Select(
+                    id="simple-select",
+                    required=True,
+                    items=[
+                        {"value": "thing", "label": "Thing", "sub": "another"},
+                        {
+                            "value": "thing-2",
+                            "label": "Second Thing",
+                            "sub": "2nd",
+                            "href": "/blah/thing",
+                        },
+                    ],
+                )
+            ]
+        ),
+        html.Div(id="select-output"),
+        html.Div(id="formgroup-valid"),
+        html.Div(id="formgroup-output"),
+    ]
+)
+
+
+@app.callback(
+    Output("select-output", "children"), [Input("simple-select", "value"), Input("simple-select", "valid")]
+)
+def update_output(value, is_valid):
+    return "you selected {}, {}".format(value, is_valid)
+
+
+@app.callback(
+    Output("formgroup-valid", "children"), [Input("formgroup", "valid")]
+)
+def update_output(valid):
+    return str(valid)
+
+
+@app.callback(
+    Output("formgroup-output", "children"), [Input("formgroup", "value")]
+)
+def update_output(value):
+    return str(value)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
---
+++ tests/test_select.py
@@ -0,0 +1,30 @@
+from dash.testing.application_runners import import_app
+
+
+def test_render_simple_radiogroup(dash_duo):
+    
+    app = import_app('tests.app.select')
+    dash_duo.start_server(app)
+
+    
+    dash_duo.wait_for_text_to_equal("#select-output", "you selected None, False")
+    dash_duo.wait_for_text_to_equal("#formgroup-valid", "False")
+
+    # Click on dropdown
+    dash_duo.multiple_click(".bp3-button", 1)
+    menu_items = dash_duo.find_elements('.bp3-menu-item')
+
+    dash_duo.multiple_click(menu_items[0], 1)
+    dash_duo.wait_for_text_to_equal("#select-output", "you selected thing, True")
+    dash_duo.wait_for_text_to_equal("#formgroup-valid", "True")
+    dash_duo.wait_for_text_to_equal("#formgroup-output", "{'simple-select': 'thing'}")
+
+    # Click on dropdown
+    dash_duo.multiple_click(".bp3-button", 1)
+    menu_items = dash_duo.find_elements('.bp3-menu-item')
+
+    dash_duo.multiple_click(menu_items[1], 1)
+    dash_duo.wait_for_text_to_equal("#select-output", "you selected thing-2, True")
+    dash_duo.wait_for_text_to_equal("#formgroup-valid", "True")
+    dash_duo.wait_for_text_to_equal("#formgroup-output", "{'simple-select': 'thing-2'}")
+
