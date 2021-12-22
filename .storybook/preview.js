79f6543a82138e8a0a02fde2f8a919f222f42611
Ben Weise <ben.weise@gmail.com>
2021-12-22 12:35:52 +1100

    Update css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,16 @@
-import "../dash_blueprint/blueprint.css";
+// import '../dash_blueprint/blueprint.css';
+// import '../dash_blueprint/blueprint-datetime.css';
+// import '../dash_blueprint/blueprint-docs.css';
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: {argTypesRegex: '^on[A-Z].*'},
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-}
+};
